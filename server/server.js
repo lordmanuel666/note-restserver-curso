732ac66b55234fb6bf8ac062df05ec72190a1bc8
Manuel <manuel.silva95@yahoo.com.mx>
2019-10-23 11:49:12 -0500

    primer commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+    res.json('Hello World!')
+})
+
+app.post('/usuarios', function(req, res) {
+    let body = req.body;
+    res.json({
+        body
+    })
+})
+
+app.put('/usuarios/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+})
+
+app.delete('/usuarios', function(req, res) {
+    res.json('Hello delete!')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('ecuchando puerto', process.env.PORT);
+});
